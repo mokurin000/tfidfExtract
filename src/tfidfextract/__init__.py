4f722cb53466d875fd0d79c97109772fd4a911e0
poly000 <1348292515a@gmail.com>
2024-10-29 00:16:42 +0800

    fix: skip parquet file in BASE_DIR

--- src/tfidfextract/__init__.py
+++ src/tfidfextract/__init__.py
@@ -2,4 +2,7 @@ import os
 
 
 BASE_DIR = "./data"
-target_dirs = map(lambda d: os.path.join(BASE_DIR, d), os.listdir(BASE_DIR))
+target_dirs = filter(
+    lambda p: os.path.isdir(p),
+    map(lambda d: os.path.join(BASE_DIR, d), os.listdir(BASE_DIR)),
+)
